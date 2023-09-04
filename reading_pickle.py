94cba34d010b295fbe8023af89afda3008cf1df3
j4t123@naver.com <j4t123@naver.com>
2023-09-04 12:38:17 +0900

    modified

--- reading_pickle.py
+++ reading_pickle.py
@@ -6,14 +6,20 @@ dir = "./pickle_file"
 algorithm = []
 for n in range(len(os.listdir(dir))):
     algorithm.append(os.listdir(dir)[n].split(',')[1])
-x = np.linspace(0, 300000, 30000)
+x = np.linspace(0, 500000, 50000)
+y_limit = 0
 for algo in algorithm:
     with open(dir+"/map:1,"+algo+",visitation_plot.pickle", "rb") as f:
         data = np.array(pickle.load(f))
-        plt.plot(x, data.mean(axis = 0), label=algo.upper())
-        plt.fill_between(x, data.mean(axis = 0)-data.std(axis = 0), data.mean(axis = 0)+data.std(axis = 0), alpha=0.3)
+        try:
+            plt.plot(x, data.mean(axis = 0), label=algo.upper())
+            plt.fill_between(x, data.mean(axis = 0)-data.std(axis = 0), data.mean(axis = 0)+data.std(axis = 0), alpha=0.3)
+            y_limit = max(y_limit,max(data.mean(axis = 0)+data.std(axis = 0)))
+        except:
+            pass
 
 plt.xlim(x[0],x[-1])
+plt.ylim(0,y_limit*1.1)
 plt.xlabel('steps')
 plt.ylabel('Mean Number of Visited Blocks')
 plt.legend()
